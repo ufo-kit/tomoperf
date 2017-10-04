3162b64e1b0cece4bccfd0982eb820c486e5f5b1
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2017-10-04 15:51:33 +0200

    astra: use 3D reconstruction

--- drivers/astr.py
+++ drivers/astr.py
@@ -14,25 +14,19 @@ def main():
     args = parser.parse_args()
     angles = np.linspace(0, np.pi, args.num_projections, False, dtype=np.float64)
 
-    reco = np.zeros((args.width, args.width), dtype=np.float32)
-    vol_geom = astra.create_vol_geom(args.width, args.width)
-    vid = astra.data2d.link('-vol', vol_geom, reco)
+    vol_geom = astra.create_vol_geom(args.width, args.width, args.num_slices)
+    proj_geom = astra.create_proj_geom('parallel3d', 1.0, 1.0, args.num_slices, args.width, angles)
 
-    proj_geom = astra.create_proj_geom('parallel', 1.0, args.width, angles)
-    pid = astra.create_projector('cuda', proj_geom, vol_geom)
+    vid = astra.data3d.create('-vol', vol_geom)
+    cube = np.zeros((args.num_slices, args.width, args.width), dtype=np.float32)
+    pid, proj_data = astra.create_sino3d_gpu(cube, proj_geom, vol_geom)
 
-    sino = np.zeros((args.num_projections, args.width), dtype=np.float32)
-    sid = astra.data2d.link('-sino', proj_geom, sino)
-
-    config = astra.astra_dict('FBP_CUDA')
-    config['ProjectorId'] = pid
-    config['ProjectionDataId'] = sid
+    config = astra.astra_dict('BP3D_CUDA')
+    config['ProjectionDataId'] = pid
     config['ReconstructionDataId'] = vid
 
     aid = astra.algorithm.create(config)
-
-    for i in range(args.num_slices):
-        astra.algorithm.run(aid)
+    astra.algorithm.run(aid)
 
 
 if __name__ == '__main__':
