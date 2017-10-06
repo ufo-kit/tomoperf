7b37926b3fc3664b64ec8b2329972ec071fe7161
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2017-10-06 12:51:52 +0200

    Add prepare command and enable lprec again

--- drivers/lprec.py
+++ drivers/lprec.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--prepare', action='store_true')
     parser.add_argument('--width', type=int, required=True)
     parser.add_argument('--num-projections', type=int, required=True)
     parser.add_argument('--num-slices', type=int, required=True)
@@ -12,10 +13,12 @@ def main():
     args = parser.parse_args()
     R = np.zeros((args.num_slices, args.width, args.num_projections), dtype=np.float32)
     handle = lprecmods.lpTransform.lpTransform(args.width, args.num_projections, args.num_slices, 'hamming', True)
-    handle.precompute()
-    handle.initcmem()
 
-    frec = handle.adj(R, args.width / 2 - 4)
+    if args.prepare:
+        handle.precompute()
+    else:
+        handle.initcmem()
+        frec = handle.adj(R, args.width / 2 - 4)
 
 
 if __name__ == '__main__':
