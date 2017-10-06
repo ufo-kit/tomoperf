7a5fe153f24d25693d6bfd2c1688c18743cb95fe
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2017-10-06 14:39:25 +0200

    Cache output of lprec

--- drivers/lprec.py
+++ drivers/lprec.py
@@ -1,4 +1,6 @@
+import os
 import argparse
+import shutil
 import lprecmods.lpTransform
 import numpy as np
 
@@ -15,7 +17,21 @@ def main():
     handle = lprecmods.lpTransform.lpTransform(args.width, args.num_projections, args.num_slices, 'hamming', True)
 
     if args.prepare:
-        handle.precompute()
+        # We cache the intermediate output for later use
+        suffix = '{}-{}'.format(args.width, args.num_projections)
+        padj_name = '/tmp/Padj-{}'.format(suffix)
+        pfwd_name = '/tmp/Pfwd-{}'.format(suffix)
+        pgl_name = '/tmp/Pgl-{}'.format(suffix)
+
+        if os.path.exists(padj_name) and os.path.exists(pfwd_name) and os.path.exists(pgl_name):
+            shutil.copyfile(padj_name, 'Padj')
+            shutil.copyfile(pfwd_name, 'Pfwd')
+            shutil.copyfile(pgl_name, 'Pgl')
+        else:
+            handle.precompute()
+            shutil.copyfile('Padj', padj_name)
+            shutil.copyfile('Pfwd', pfwd_name)
+            shutil.copyfile('Pgl', pgl_name)
     else:
         handle.initcmem()
         frec = handle.adj(R, args.width / 2 - 4)
